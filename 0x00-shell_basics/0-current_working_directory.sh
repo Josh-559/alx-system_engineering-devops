1777aa4127f37107047d0e638d191b5544c325a8
Samuel Ifeanyi <samuelify225@gmail.com>
2023-02-08 15:18:16 -0800

    alx-system_engineering-devops first commit

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Store the current working directory in a variable
+current_directory=$(pwd)
+
+# Print the absolute path name of the current working directory
+echo "$current_directory"
+
